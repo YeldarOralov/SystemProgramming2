f6a66d4840630d3c94a9cdb1d423fd7425fb5e3c
YeldarOralov <YeldarOralov@users.noreply.github.com>
2019-05-28 11:53:45 +0600

    Добавлены файлы проекта

---
+++ ExeForTestingDownload/ExeForTestingDownload/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExeForTestingDownload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var client = new WebClient())
+            {
+                client.DownloadFile("ftp://videodarom:1169@videodarom.no-ip.org/Alpha.2018.BDRip.VideoDarom.ru.mkv", "Film");
+            }
+        }
+    }
+}
---
+++ TestingWork/TestingWork/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestingWork
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        AppDomain newDomain;
+        AppDomain currentDomain;
+        string assemblyForNewDomain;
+        public MainWindow()
+        {
+            InitializeComponent();
+            currentDomain = AppDomain.CurrentDomain;
+            newDomain = AppDomain.CreateDomain("DomainForTest");
+        }
+
+        private void DownladButtonClick(object sender, RoutedEventArgs e)
+        {
+            assemblyForNewDomain = currentDomain.BaseDirectory + "ExeForTestingDownload.exe";
+            newDomain.ExecuteAssembly(assemblyForNewDomain);
+        }
+
+        private void LoopButtonClick(object sender, RoutedEventArgs e)
+        {
+            assemblyForNewDomain = currentDomain.BaseDirectory + "ExeForTestingLoop.exe";
+            newDomain.ExecuteAssembly(assemblyForNewDomain);
+        }
+    }
+}
